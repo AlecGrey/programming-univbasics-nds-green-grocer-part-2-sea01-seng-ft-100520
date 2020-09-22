0700cce7e1bab00a411e3c8bbce3572292b2a5fc
Alexander Grey <alecgrey.coding@gmail.com>
2020-09-22 19:17:42 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -14,7 +14,6 @@ def apply_coupons(cart, coupons)
     cart.each do |current|
       if current[:item] == coupon[:item] && current[:count] >= coupon[:num]
         current[:count] -= coupon[:num]
-        binding.pry
         cart >> {
           :item => "#{coupon[:item]} W/COUPON",
           :price => coupon[:cost] / coupon[:num],
