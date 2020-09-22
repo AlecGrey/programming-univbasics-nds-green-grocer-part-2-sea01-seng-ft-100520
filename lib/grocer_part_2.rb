6ada4e3080fd10392a22f4c225ef52fda689f8a8
Alexander Grey <alecgrey.coding@gmail.com>
2020-09-22 20:08:31 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -37,7 +37,6 @@ def apply_clearance(cart)
 end
 
 def checkout(cart, coupons)
-  
   new_cart = consolidate_cart(cart)
   new_cart_with_coupons = apply_coupons(new_cart, coupons)
   new_cart_with_discounts = apply_clearance(new_cart_with_coupons)
